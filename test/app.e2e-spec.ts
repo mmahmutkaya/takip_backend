7125936bb4ac62c43e110fb0cb8e86242becdf6c
Mahmut Kaya <mmahmutkaya@gmail.com>
2026-03-06 17:08:02 +0300

    bb

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
+  // delete me
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
